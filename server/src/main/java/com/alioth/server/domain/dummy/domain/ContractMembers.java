a1a177c1397fbdb83f5f3388fdd1a450bfdaec19
GyeongNam <jo_gn97@naver.com>
2024-03-26 00:36:39 +0900

    [chore] : 더미 데이터 엔티티 생성 완료
    1. 각 더미 데이터 엔티티 파일생성 및 FK 설정
    ```
    @ManyToOne
    @JoinColumn(name = "entity_pk")
    private entity entity_name;
    ```
    으로 1:N 설정

--- server/src/main/java/com/alioth/server/domain/dummy/domain/ContractMembers.java
+++ server/src/main/java/com/alioth/server/domain/dummy/domain/ContractMembers.java
@@ -2,8 +2,23 @@ package com.alioth.server.domain.dummy.domain;
 
 
 import com.alioth.server.common.domain.BaseEntity;
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
 @Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ContractMembers extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long CM_Id;
+    @Column(nullable = false)
+    private String CM_code;
+    @Column(nullable = false)
+    private String CM_name;
 }
--- server/src/main/java/com/alioth/server/domain/dummy/domain/InsuranceProduct.java
+++ server/src/main/java/com/alioth/server/domain/dummy/domain/InsuranceProduct.java
@@ -1,10 +1,7 @@
 package com.alioth.server.domain.dummy.domain;
 
 import com.alioth.server.common.domain.BaseEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,12 +16,12 @@ public class InsuranceProduct extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long insuranceId;
-
+    @Column(nullable = false)
     private String insuranceName;
-
+    @Column(nullable = false)
     private String insuranceCategory;
-
+    @Column(nullable = false)
     private String insuranceSubCode;
-
+    @Column(nullable = false)
     private String insuranceMainCode;
 }
