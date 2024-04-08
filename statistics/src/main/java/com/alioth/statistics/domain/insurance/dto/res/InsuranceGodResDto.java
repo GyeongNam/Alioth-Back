e5bf63e0facd272968b142364fd3cce12e97dcd9
Park Sung Jun <junstin119@gmail.com>
2024-04-08 14:13:27 +0900

    [feat]: 보험의 신 작성 - 이번달 보험의 신 - 분기별 보험의 신 - 올해의 보험의 신

---
+++ statistics/src/main/java/com/alioth/statistics/domain/insurance/dto/res/InsuranceGodResDto.java
@@ -0,0 +1,20 @@
+package com.alioth.statistics.domain.insurance.dto.res;
+
+import lombok.Builder;
+
+@Builder
+public record InsuranceGodResDto(
+        String memberCode,
+        String memberName,
+        String totalPrice,
+        Long Count,
+        String CancelAvgPrice,
+        String performanceReview
+) {
+
+    public void sortedDto() {
+
+    }
+
+
+}
